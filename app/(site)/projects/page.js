f3422d6a4ad6429093b8ed8e73ae0c0ce133cc1d
Zahira <zahiraanindyaputri@mail.ugm.ac.id>
2025-11-01 12:06:48 +0700

    change url project

--- app/(site)/projects/page.js
+++ app/(site)/projects/page.js
@@ -105,7 +105,7 @@ export default function ProjectsPage() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const res = await fetch("https://webperszahira.vercel.app/api/projects?depth=2");
+        const res = await fetch(`${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/projects?depth=2`);
         const data = await res.json();
         setProjects(data?.docs || []);
       } catch (err) {
