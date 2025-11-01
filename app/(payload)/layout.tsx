b175760bf5a356f8aeb5ac891b64a3752945f328
Zahira <zahiraanindyaputri@mail.ugm.ac.id>
2025-11-01 07:53:56 +0700

    Fix: type Args di PayloadLayout

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,24 +1,22 @@
 import config from "@payload-config";
 import "@payloadcms/next/css";
-import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import { RootLayout } from "@payloadcms/next/layouts";
 import type { ServerFunctionClient } from "payload";
 import React from "react";
 import { importMap } from "./admin/importMap.js";
 
 type Args = {
   children: React.ReactNode;
+  config: typeof config; 
+  importMap: typeof importMap; 
+  serverFunction: ServerFunctionClient;
 };
 
-const serverFunction: ServerFunctionClient = async function (args) {
-  "use server";
-  return handleServerFunctions({
-    ...args,
-    config,
-    importMap,
-  });
-};
-
-export default function PayloadLayout({ children }: Args) {
+export default function PayloadLayout({ 
+  children,
+  config,
+  importMap,
+  serverFunction, }: Args) {
   return (
     <RootLayout
       config={config}
