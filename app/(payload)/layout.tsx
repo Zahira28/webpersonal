d78f81a67c563b75a3c2b77bf3241fff0a4348b1
Zahira <zahiraanindyaputri@mail.ugm.ac.id>
2025-11-01 08:19:37 +0700

    Fix layout.tsx

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,22 +1,24 @@
 import config from "@payload-config";
 import "@payloadcms/next/css";
-import { RootLayout } from "@payloadcms/next/layouts";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
 import type { ServerFunctionClient } from "payload";
-import type { SanitizedConfig } from 'payload';
 import React from "react";
+import { importMap } from "./admin/importMap.js";
 
 type Args = {
   children: React.ReactNode;
-  config: Promise<SanitizedConfig>; 
-  importMap: Record<string, string>; 
-  serverFunction: ServerFunctionClient;
 };
 
-export default function PayloadLayout({ 
-  children,
-  config,
-  importMap,
-  serverFunction, }: Args) {
+const serverFunction: ServerFunctionClient = async function (args) {
+  "use server";
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  });
+};
+
+export default function PayloadLayout({ children }: Args) {
   return (
     <RootLayout
       config={config}
