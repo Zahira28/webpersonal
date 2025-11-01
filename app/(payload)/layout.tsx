f30c3a577e0335ef42651712a1dbf5fc9d639910
Zahira <zahiraanindyaputri@mail.ugm.ac.id>
2025-11-01 10:27:43 +0700

    fix layout.tsx lagi

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -24,8 +24,8 @@ export default function PayloadLayout({ children }: Args) {
       config={config}
       importMap={importMap}
       serverFunction={serverFunction}
+      children={children}
     >
-      {children}
     </RootLayout>
   );
 }
